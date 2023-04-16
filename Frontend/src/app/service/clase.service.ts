64fe31f9867b17486dba4f69f9ff6ab6dd41be97
mictlangelo10 <123588416+mictlangelo10@users.noreply.github.com>
2023-04-16 11:08:14 -0600

    Corrigiendo Documentación

--- Frontend/src/app/service/clase.service.ts
+++ Frontend/src/app/service/clase.service.ts
@@ -12,27 +12,35 @@ export class ClaseService {
   claseURL = environment.apiRestURL + '/Clase';
   constructor(private http: HttpClient) {}
 
+  // Método lista de las clases
   public list(): Observable<Clase[]> {
     return this.http.get<Clase[]>(this.claseURL);
   }
 
+  // Método detalles de las clases
   public detail(id: number): Observable<Clase> {
     return this.http.get<Clase>(this.claseURL + `/${id}`);
   }
 
+  // Método clase
   public obtenerClase(id: string): Observable<any> {
     return this.http.get(this.claseURL + id);
   }
 
+  // Método creación de clase
   public create(clase: Clase): Observable<any> {
     return this.http.post<any>(this.claseURL, clase);
   }
 
+  // Método actualización de clase
   public update(id: number, clase: Clase): Observable<any> {
     return this.http.put<any>(this.claseURL + `/${id}`, clase);
   }
 
+  // Método eliminación de clase
   public delete(id: number): Observable<any> {
     return this.http.delete<any>(this.claseURL + `/${id}`);
   }
 }
+
+// Autor: Miguel Hernández
