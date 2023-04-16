9248e4cb4a3542a8b0105892d1af6a6bb5c72792
mictlangelo10 <123588416+mictlangelo10@users.noreply.github.com>
2023-04-16 11:11:30 -0600

    Corrigiendo Documentación

--- Frontend/src/app/service/cliente.service.ts
+++ Frontend/src/app/service/cliente.service.ts
@@ -9,31 +9,40 @@ import { Clientes } from '../models/clientes';
 })
 export class ClienteService {
 
+  // Declarando Variables
   clienteURL=environment.apiRestURL+'/Cliente';
   constructor(private httpClient:HttpClient) { }
 
+  // Método para obtener clientes
   public list():Observable<Clientes[]>{
     return this.httpClient.get<Clientes[]>(this.clienteURL);
   }
 
+  // Método para obtener detalles de los clientes
   public detail(id: number):Observable<Clientes>{
     return this.httpClient.get<Clientes>(this.clienteURL + `/${id}`);
   }
 
+  // Método para obtener clientes por medio de su nombre
   public listByNombreClase(nombreClase: String):Observable<Clientes[]>{
     return this.httpClient.get<Clientes[]>(this.clienteURL+ `/NombreClase/${nombreClase}`);
   }
 
+  // Método para agregar clientes
   public save(cliente:Clientes):Observable<any>{
     return this.httpClient.post<any>(this.clienteURL,cliente);
   }
 
+  //  Método para actualizar clientes
   public update(id: number,cliente:Clientes):Observable<any>{
     return this.httpClient.put<any>(this.clienteURL+`/${id}`,cliente);
   }
 
+  // Método para eliminar clientes
   public delete(id: number):Observable<any>{
     return this.httpClient.delete<any>(this.clienteURL+`/${id}`);
   }
   
 }
+
+// Autor: Miguel Hernández
